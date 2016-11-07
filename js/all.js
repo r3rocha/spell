237620283f45282f97435696a815f4d8eb09952c
Hugo Lopes Tavares <hltbra@gmail.com>
2016-11-06 23:19:56 -0500

    Save current level to the database and load it when the page is loaded

--- js/all.js
+++ js/all.js
@@ -19,15 +19,47 @@ function signup_user(user, pass, secret) {
         database.ref("/users/" + result.uid).set({
             username: user,
             coins: 0,
+            current_level: "easy"
         });
     }).catch(function(error) {
         console.log(error);
     });
 }
 
-function setup_coins() {
+function setup_coins($elem) {
     var uid = firebase.auth().currentUser.uid;            
-    database.ref("users/" + uid + "/coins").on('value', function(result) {
-        $("#usercoins .coins").html(result.val());
+    database.ref("/users/" + uid + "/coins").on('value', function(result) {
+        $elem.html(result.val());
+    });
+}
+
+function setup_level($elems) {
+    get_current_level(function (current_level) {
+        console.log("current_level", current_level);
+        var $current_level = $elems.filter("[data-level=" + current_level + "]");
+        select_level($current_level, $elems);
+    });
+    $elems.on('click', function() {
+        select_level($(this), $elems);
+        var level = $(this).attr("data-level");
+        save_level(level);
+    });
+}
+
+function select_level($elem, $elems) {
+    $elems.removeClass("active");
+    $elems.addClass("inactive");
+    $elem.removeClass("inactive").addClass("active");
+}
+
+function save_level(level) {
+    var uid = firebase.auth().currentUser.uid;
+    database.ref("/users/" + uid + "/current_level").set(level);
+}
+
+function get_current_level(callback) {
+    var uid = firebase.auth().currentUser.uid;
+    database.ref("/users/" + uid + "/current_level").once('value', function(result) {
+        callback(result.val());
     });
 }
