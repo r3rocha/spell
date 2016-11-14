56493fc314dddb942bb2d5451df11a5de5fed5c7
Hugo Lopes Tavares <hltbra@gmail.com>
2016-11-13 19:37:30 -0500

    Move sound functions to OnOff state prototype object

--- js/all.js
+++ js/all.js
@@ -71,43 +71,59 @@ function get_current_level(callback) {
 
 /* sound effects */
 function play_bubble() {
-    if (is_sound_on()) {
+    // assume `sound` is a global var
+    if (sound.is_on()) {
         var audio = new Audio("sound/effects/bubble.mp3");
         audio.play();
     }
 }
 
-function is_sound_on() {
-    return !("spell_game:sound_state" in localStorage) || localStorage.getItem("spell_game:sound_state") == "on";
-}
+/* sound */
 
-function set_sound_off($elem) {
-    console.log("sound off");
-    $elem.addClass("sound-off");
-    return localStorage.setItem("spell_game:sound_state", "off");
+OnOffState = function(name, $elem) {
+    this.name = name;
+    this.local_storage_name = "spell_game:" + this.name + "_state";
+    this.$elem = $elem;
 }
+OnOffState.prototype.is_on = function() {
+    return (!(this.local_storage_name in localStorage) ||
+            localStorage.getItem(this.local_storage_name) == "on");
 
-function set_sound_on($elem) {
-    console.log("sound on");
-    $elem.removeClass("sound-off");
-    return localStorage.setItem("spell_game:sound_state", "on");
 }
-
-function toggle_sound($elem) {
-    if ($elem.hasClass('sound-off')) {
-        set_sound_on($elem);
+OnOffState.prototype.turn_off = function() {
+    console.log(this.name, "off");
+    this.$elem.addClass(this.name + "-off");
+    return localStorage.setItem(this.local_storage_name, "off");
+}
+OnOffState.prototype.turn_on = function() {
+    console.log(this.name, "on");
+    this.$elem.removeClass(this.name + "-off");
+    return localStorage.setItem(this.local_storage_name, "on");
+}
+OnOffState.prototype.toggle = function() {
+    if (this.is_on()) {
+        this.turn_off();
     } else {
-        set_sound_off($elem);
+        this.turn_on();
     }
 }
-
-function setup_sound($elem) {
-    if (is_sound_on()) {
-        set_sound_on($elem);
-    } else {
-        set_sound_off($elem);
-    }
-    $elem.on('click', function() {
-        toggle_sound($(this));
+OnOffState.prototype.setup_click = function() {
+    var self = this;
+    this.$elem.on('click', function() {
+        self.toggle();
     });
+}
+OnOffState.prototype.setup_initial_state = function() {
+    if (this.is_on()) {
+        this.turn_on();
+    } else {
+        this.turn_off();
+    }
+
+}
+function setup_sound($elem) {
+    var sound = new OnOffState("sound", $elem);
+    sound.setup_initial_state();
+    sound.setup_click();
+    return sound;
 }
