162870d181af8d061692b4b473433a9d9555a561
Renata Rocha <renat4@gmail.com>
2016-11-29 15:54:24 -0500

    #13 Add more words to spell - [ ] create sounds in English - [ ] create sound in Spanish - [ ] create related images - [ ] add words, images and related sounds to dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -5,15 +5,11 @@ var WORDS = {
             "image": "images/words/turtle.svg",
             "word": {
                 "en-us": "turtle",
-                "es-sp": "tortuga",
-                "ptbr": "",
-                "it": ""
+                "es-sp": "tortuga"
             },
             "sound": {
                 "en-us": "sound/en-us/turtle.mp3",
-                "es-sp": "sound/es-sp/tortuga.mp3",
-                "ptbr": "",
-                "it": ""
+                "es-sp": "sound/es-sp/tortuga.mp3"
             },
         },
         {
@@ -21,16 +17,327 @@ var WORDS = {
             "image": "images/words/giraffe.svg",
             "word": {
                 "en-us": "giraffe",
-                // "es-sp": "tortuga",
-                // "ptbr": "",
-                // "it": ""
+                "es-sp": "jirafa"
             },
             "sound": {
                 "en-us": "sound/en-us/giraffe.mp3",
-                // "es-sp": "sound/es-sp/tortuga.mp3",
-                // "ptbr": "",
-                // "it": ""
+                "es-sp": "sound/es-sp/jirafa.mp3"
             },
+        },
+        {
+            "alt": "Penguin",
+            "image": "images/words/penguin.svg",
+            "word": {
+                "en-us": "penguin",
+                "es-sp": "pingüino"
+            },
+            "sound": {
+                "en-us": "sound/en-us/penguin.mp3",
+                "es-sp": "sound/es-sp/pinguino.mp3"
+            },
+        },
+        {
+            "alt": "Pig",
+            "image": "images/words/pig.svg",
+            "word": {
+                "en-us": "pig",
+                "es-sp": "cerdo"
+            },
+            "sound": {
+                "en-us": "sound/en-us/pig.mp3",
+                "es-sp": "sound/es-sp/cerdo.mp3"
+            },
+        },
+        {
+            "alt": "Hippo",
+            "image": "images/words/hippo.svg",
+            "word": {
+                "en-us": "hippo",
+                "es-sp": "hipopótamo"
+            },
+            "sound": {
+                "en-us": "sound/en-us/hippo.mp3",
+                "es-sp": "sound/es-sp/hipopotamo.mp3"
+            },
+        },
+        {
+            "alt": "Sheep",
+            "image": "images/words/sheep.svg",
+            "word": {
+                "en-us": "sheep",
+                "es-sp": "oveja"
+            },
+            "sound": {
+                "en-us": "sound/en-us/sheep.mp3",
+                "es-sp": "sound/es-sp/oveja.mp3"
+            }
+        }
+    ], 
+    "kitchen":[
+        {
+            "alt": "Apron",
+            "image": "images/words/apron.svg",
+            "word": {
+                "en-us": "apron",
+                "es-sp": "delantal"
+            },
+            "sound": {
+                "en-us": "sound/en-us/apron.mp3",
+                "es-sp": "sound/es-sp/delantal.mp3"
+            },
+        },
+        {
+            "alt": "toaster",
+            "image": "images/words/toaster.svg",
+            "word": {
+                "en-us": "toaster",
+                "es-sp": "tostadora"
+            },
+            "sound": {
+                "en-us": "sound/en-us/toaster.mp3",
+                "es-sp": "sound/es-sp/tostadora.mp3"
+            },
+        },
+        {
+            "alt": "refrigerator",
+            "image": "images/words/refrigerator.svg",
+            "word": {
+                "en-us": "refrigerator",
+                "es-sp": "nevera"
+            },
+            "sound": {
+                "en-us": "sound/en-us/refrigerator.mp3",
+                "es-sp": "sound/es-sp/nevera.mp3"
+            },
+        },
+        {
+            "alt": "blender",
+            "image": "images/words/blender.svg",
+            "word": {
+                "en-us": "blender",
+                "es-sp": "licuadora"
+            },
+            "sound": {
+                "en-us": "sound/en-us/blender.mp3",
+                "es-sp": "sound/es-sp/licuadora.mp3"
+            },
+        },
+        {
+            "alt": "pan",
+            "image": "images/words/pan.svg",
+            "word": {
+                "en-us": "pan",
+                "es-sp": "sartén"
+            },
+            "sound": {
+                "en-us": "sound/en-us/pan.mp3",
+                "es-sp": "sound/es-sp/sartén.mp3"
+            },
+        },
+        {
+            "alt": "microwave",
+            "image": "images/words/microwave.svg",
+            "word": {
+                "en-us": "microwave",
+                "es-sp": "microonda"
+            },
+            "sound": {
+                "en-us": "sound/en-us/microwave.mp3",
+                "es-sp": "sound/es-sp/microonda.mp3"
+            }
+        }
+    ], 
+    "food":[
+        {
+            "alt": "apple",
+            "image": "images/words/apple.svg",
+            "word": {
+                "en-us": "apple",
+                "es-sp": "manzano"
+            },
+            "sound": {
+                "en-us": "sound/en-us/apple.mp3",
+                "es-sp": "sound/es-sp/manzano.mp3"
+            },
+        },
+        {
+            "alt": "burger",
+            "image": "images/words/burger.svg",
+            "word": {
+                "en-us": "burger",
+                "es-sp": "hamburguesa"
+            },
+            "sound": {
+                "en-us": "sound/en-us/burger.mp3",
+                "es-sp": "sound/es-sp/hamburguesa.mp3"
+            },
+        },
+        {
+            "alt": "coffee",
+            "image": "images/words/coffee.svg",
+            "word": {
+                "en-us": "coffee",
+                "es-sp": "café"
+            },
+            "sound": {
+                "en-us": "sound/en-us/coffee.mp3",
+                "es-sp": "sound/es-sp/cafe.mp3"
+            },
+        },
+        {
+            "alt": "egg",
+            "image": "images/words/egg.svg",
+            "word": {
+                "en-us": "egg",
+                "es-sp": "huevos"
+            },
+            "sound": {
+                "en-us": "sound/en-us/egg.mp3",
+                "es-sp": "sound/es-sp/huevos.mp3"
+            },
+        },
+        {
+            "alt": "cheese",
+            "image": "images/words/cheese.svg",
+            "word": {
+                "en-us": "cheese",
+                "es-sp": "queso"
+            },
+            "sound": {
+                "en-us": "sound/en-us/cheese.mp3",
+                "es-sp": "sound/es-sp/queso.mp3"
+            },
+        },
+        {
+            "alt": "French fries",
+            "image": "images/words/frenchfries.svg",
+            "word": {
+                "en-us": "French fries",
+                "es-sp": "papas fritas"
+            },
+            "sound": {
+                "en-us": "sound/en-us/frenchfries.mp3",
+                "es-sp": "sound/es-sp/papasfritas.mp3"
+            }
+        },
+        {
+            "alt": "bacon",
+            "image": "images/words/bacon.svg",
+            "word": {
+                "en-us": "bacon",
+                "es-sp": "tocino"
+            },
+            "sound": {
+                "en-us": "sound/en-us/bacon.mp3",
+                "es-sp": "sound/es-sp/tocino.mp3"
+            }
+        },
+        {
+            "alt": "pancakes",
+            "image": "images/words/pancakes.svg",
+            "word": {
+                "en-us": "pancakes",
+                "es-sp": "panqueques"
+            },
+            "sound": {
+                "en-us": "sound/en-us/pancakes.mp3",
+                "es-sp": "sound/es-sp/panqueques.mp3"
+            }
+        }
+    ],
+    "clothes":[
+        {
+            "alt": "dress",
+            "image": "images/words/dress.svg",
+            "word": {
+                "en-us": "dress",
+                "es-sp": "vestido"
+            },
+            "sound": {
+                "en-us": "sound/en-us/dress.mp3",
+                "es-sp": "sound/es-sp/vestido.mp3"
+            },
+        },
+        {
+            "alt": "pajamas",
+            "image": "images/words/pajamas.svg",
+            "word": {
+                "en-us": "pajamas",
+                "es-sp": "pijama"
+            },
+            "sound": {
+                "en-us": "sound/en-us/pajamas.mp3",
+                "es-sp": "sound/es-sp/pijama.mp3"
+            },
+        },
+        {
+            "alt": "coat",
+            "image": "images/words/coat.svg",
+            "word": {
+                "en-us": "coat",
+                "es-sp": "abrigo"
+            },
+            "sound": {
+                "en-us": "sound/en-us/coat.mp3",
+                "es-sp": "sound/es-sp/abrigo.mp3"
+            },
+        },
+        {
+            "alt": "pants",
+            "image": "images/words/pants.svg",
+            "word": {
+                "en-us": "pants",
+                "es-sp": "pantalones"
+            },
+            "sound": {
+                "en-us": "sound/en-us/pants.mp3",
+                "es-sp": "sound/es-sp/pantalones.mp3"
+            },
+        },
+        {
+            "alt": "hat",
+            "image": "images/words/hat.svg",
+            "word": {
+                "en-us": "hat",
+                "es-sp": "sombrero"
+            },
+            "sound": {
+                "en-us": "sound/en-us/hat.mp3",
+                "es-sp": "sound/es-sp/sombrero.mp3"
+            },
+        },
+        {
+            "alt": "socks",
+            "image": "images/words/socks.svg",
+            "word": {
+                "en-us": "socks",
+                "es-sp": "calcetines"
+            },
+            "sound": {
+                "en-us": "sound/en-us/socks.mp3",
+                "es-sp": "sound/es-sp/calcetines.mp3"
+            }
+        },
+        {
+            "alt": "skirt",
+            "image": "images/words/skirt.svg",
+            "word": {
+                "en-us": "skirt",
+                "es-sp": "falda"
+            },
+            "sound": {
+                "en-us": "sound/en-us/skirt.mp3",
+                "es-sp": "sound/es-sp/falda.mp3"
+            }
         }
     ]
 };
+
+
+
+
+
+
+
+
+
