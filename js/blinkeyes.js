82d45ed2ba509db90d1c9a31970c62d070965bf6
Renata Rocha <renat4@gmail.com>
2016-11-29 20:45:21 -0500

    Load dog-home.svg at home.html - add snap.svg script - change html - adjust css

---
+++ js/blinkeyes.js
@@ -0,0 +1,15 @@
+
+
+//Blink Eyes
+// function blinkEyes() {
+//     if ($("#logo .eyes-open").size()) {
+//         $(".eyes-open").attr("src","images/logo2.svg").removeClass("eyes-open").addClass("eyes-closed");
+
+//     } else {
+
+//         $(".eyes-closed").attr("src","images/logo.svg").removeClass("eyes-closed").addClass("eyes-open");
+//     } 
+// }
+
+// setInterval(blinkEyes,400); setInterval(blinkEyes,800); setInterval(blinkEyes,500);
+
